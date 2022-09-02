3c85f640aca44971e49969dd67d2b506e589a138
Noah <lauffern@gmail.com>
2022-09-02 12:21:32 -0300

    Custom user Model allows for superuser creation

--- app/core/models.py
+++ app/core/models.py
@@ -9,12 +9,24 @@ from django.contrib.auth.models import (
     PermissionsMixin,
 )
 
+
 class UserManager(BaseUserManager):
     """ Manager for users """
     def create_user(self, email, password=None, **extra_field):
         """ Create save and return a new user """
-        user = self.model(email=email, **extra_field)
+        if not email:
+            raise ValueError('User must enter a valid email')
+        user = self.model(email=self.normalize_email(email), **extra_field)
         user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """ Creates and returns a SuperUser """
+        user = self.create_user(email, password)
+        user.is_superuser = True
+        user.is_staff = True
         user.save(using=self._db)
 
         return user
