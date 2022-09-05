c289381a39b1af5a8201dc6ad01c8258f95ced04
NoahL <lauffern@gmail.com>
2022-09-05 11:15:32 -0300

    implement create user API and tests

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularSwaggerView,
@@ -27,5 +28,6 @@ urlpatterns = [
         'api/docs',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs'
-        )
+        ),
+    path('api/user/', include('user.urls'))
 ]
